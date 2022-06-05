d01a2e8ad3ba755df6f47a1dcf33dacc00d9dba7
Rishika Bhagat <92221764+Rishikab63@users.noreply.github.com>
2022-06-05 17:12:24 +0530

    Update Sort a Stack.cpp

--- Sort a Stack.cpp
+++ Sort a Stack.cpp
@@ -3,7 +3,7 @@ void sortD(stack<int> &s,int elm){
     //basecase
 
     if(s.empty()){
-        s.push(elm);
+        s.push(elm);   //if empty, push the fetched element into the stack
         return;
     }
     int n=s.top();
@@ -22,10 +22,10 @@ void sortStack(stack<int> &stack)
 {
 	// Write your code here
     //base case-
-    if(stack.empty())
+    if(stack.empty())   //checking if its empty
         return;
     int elm=stack.top();
     stack.pop();
-    sortStack(stack);
-    sortD(stack,elm);
+    sortStack(stack);   //recursive call of the same function to fetch the element
+    sortD(stack,elm);   //another function called to sort the elements.
 }
